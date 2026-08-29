66f9d221a9c5542599a383df0c592fc9a9ab4113
lee314 <lee314@noreply.gitcode.com>
2026-08-29 20:02:22 +0800

    waterfall case modify

--- demos/04-waterfall/waterfall-demo.ts
+++ demos/04-waterfall/waterfall-demo.ts
@@ -10,15 +10,37 @@ export const name = 'waterfall-demo'
 
 export async function apply(ctx: Context) {
   ctx.on('demo/transform', async (_input, next) => {
+    console.log('listen 1 begin')
     const downstream = await next()
+
+    console.log('listen 1 end, ', downstream)
     return downstream.toUpperCase()
   })
 
   ctx.on('demo/transform', async (input, next) => {
-    if (input.includes('blocked')) return '** blocked **'
-    return next()
+    console.log('listen 2 begin')
+    
+    if (input.includes('blocked')) {
+      console.log('listen 2 blocked')
+      return '** blocked **'
+    }
+
+    const downstream = await next()
+
+    console.log('listen 2 end, ', downstream)
+    return downstream
   })
 
-  console.log(await ctx.waterfall('demo/transform', 'hello', async () => 'hello'))
-  console.log(await ctx.waterfall('demo/transform', 'blocked words', async () => 'blocked words'))
+  console.log(await ctx.waterfall('demo/transform', 'hello', async () => {
+    console.log('trigger hello')
+    return 'hello result'
+  }))
+
+  console.log('--------------------------------')
+
+  console.log(await ctx.waterfall('demo/transform', 'blocked words', async () => {
+    console.log('trigger blocked words')
+    return 'blocked words result'
+  }))
+
 }
